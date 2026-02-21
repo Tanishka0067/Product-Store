d75e45410274306c0cbf5ce73857e1962a1a792a
Tanishka Agarwal <144484154+Tanishka0067@users.noreply.github.com>
2026-02-22 00:17:49 +0530

    routes and conrollers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,9 @@ import express from "express"
 import cors from "cors";
 import { ENV } from "./config/env"
 import { clerkMiddleware } from '@clerk/express'
-
+import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
+import commentRoutes from "./routes/commentRoutes";
 const app = express()
 
 app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
@@ -21,7 +23,9 @@ app.get("/",(req,res)=>{
         }
     })
 })
-
+app.use("/api/users",userRoutes)
+app.use("/api/products",productRoutes)
+app.use("/api/comments",commentRoutes)
 app.listen(ENV.PORT,()=>{
     console.log(`Server is running on port ${ENV.PORT}`)
 }) 
